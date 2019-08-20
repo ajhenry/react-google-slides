cb515ce9daba6dcbc457c1ae143363ed9635f6c0
Andrew Henry <ajh145@pitt.edu>
2019-08-19 23:09:47 -0400

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'react-google-slides'
+import ReactGoogleSlides from "react-google-slides";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className="container">
+        <div style={{ padding: 20 }}>
+          <ReactGoogleSlides
+            width="100%"
+            slidesLink="https://docs.google.com/presentation/d/172oFC8-LBw0GQEymFDbTBn-ORh7wi2ByfUXrXm7H-AM"
+            slideDuration={5}
+          />
+        </div>
+        <div style={{ padding: 20 }}>
+          <ReactGoogleSlides
+            width="50%"
+            slidesLink="https://docs.google.com/presentation/d/1_f7J6Mu4wAy5TOkcgQREekvGvGd3V_Tt09nMmKb6T0I/edit?usp=sharing"
+            showControls
+            loop
+          />
+        </div>
+        <div style={{ padding: 20 }}>
+          <ReactGoogleSlides width="75%" slideDuration={10} showControls />
+        </div>
+        <div style={{ padding: 20 }}>
+          <ReactGoogleSlides width={300} height={300} loop />
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,100 @@
 /**
- * @class ExampleComponent
+ * @class ReactGoogleSlides
  */
 
-import * as React from 'react'
+import * as React from "react";
 
-import styles from './styles.css'
+export type Props = {
+  slidesLink: string;
+  loop: boolean;
+  slideDuration: number;
+  showControls: boolean;
+  height: string | number;
+  width: string | number;
+  containerStyle: object | null;
+};
 
-export type Props = { text: string }
+/**
+ * Generates iframe compatible url to display the presentation
+ * @param presentationKey The Google Slides presentation key
+ * @param loop Boolean for whether the slides should loop after finishing
+ * @param slideDuration Duration in seconds for how long each slide should be
+ * @param showControls Boolean for whether to display the Google Slides controls
+ */
+const constructUrl = (
+  presentationKey: string | null,
+  loop: boolean,
+  slideDuration: number,
+  showControls: boolean
+): string => {
+  if (!presentationKey) {
+    return ``;
+  }
 
-export default class ExampleComponent extends React.Component<Props> {
+  let baseUrl = "https://docs.google.com/presentation/d/";
+  baseUrl += `${presentationKey}/embed?`;
+  baseUrl += `start=true`;
+  baseUrl += `&loop=${loop ? "true" : "false"}`;
+  baseUrl += `&delayms=${slideDuration * 1000}`;
+
+  if (!showControls) {
+    baseUrl += `&rm=minimal`;
+  }
+
+  return baseUrl;
+};
+
+// Regex for extracting presentation id
+const regex: RegExp = new RegExp(
+  `(((https|http):\/\/|)docs\.google\.com\/presentation\/d\/)(.+?(?=(\/.+|\/|$)))`
+);
+
+// Extracts the slide id from the share-able url
+const extractSlidesKey = (slidesUrl: string): string | null => {
+  const match = regex.exec(slidesUrl);
+  return match ? match[4] : null;
+};
+
+// Calculates dimension for string/numbers
+const calcDimension = (dim: string | null | number): string =>
+  dim ? (typeof dim === "number" ? `${dim}px` : dim) : `480px`;
+
+export default class ReactGoogleSlides extends React.Component<Props> {
+  public static defaultProps = {
+    width: `640px`,
+    height: `480px`,
+    slidesLink: `https://docs.google.com/presentation/d/1OHEMQ0W-3iXvbxS0g86oMHlhVgKaYP7d3-CUhSHdCjs/edit?usp=sharing`,
+    loop: false,
+    slideDuration: 2,
+    showControls: false
+  };
+
   render() {
     const {
-      text
-    } = this.props
+      slidesLink,
+      loop,
+      slideDuration,
+      showControls,
+      width,
+      height,
+      containerStyle
+    } = this.props;
+    const presentationKey = extractSlidesKey(slidesLink);
+    const url = constructUrl(
+      presentationKey,
+      loop,
+      slideDuration,
+      showControls
+    );
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+      <iframe
+        src={url}
+        width={calcDimension(width)}
+        height={calcDimension(height)}
+        style={containerStyle ? containerStyle : { border: 0 }}
+        allowFullScreen={true}
+      />
+    );
   }
 }
