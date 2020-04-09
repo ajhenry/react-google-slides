79d0ffaa029f8eb86308aeaacedf0205877c89f1
Andrew Henry <ajh145@pitt.edu>
2020-04-08 22:41:44 -0400

    Updates

---
+++ precache-manifest.c9e819edd3e374f0bbf6723b0ac6195e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27505c48ccaefa0b91dc183746531f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ae33fbf0871277c00e4",
+    "url": "/static/css/main.6db09ef2.chunk.css"
+  },
+  {
+    "revision": "e1d449a91dff5f18ed68",
+    "url": "/static/js/2.cad7d14a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cad7d14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae33fbf0871277c00e4",
+    "url": "/static/js/main.10052969.chunk.js"
+  },
+  {
+    "revision": "a64a88e7db41b38045aa",
+    "url": "/static/js/runtime-main.f3a207de.js"
+  }
+]);
