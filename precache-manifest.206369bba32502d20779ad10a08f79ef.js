f486d86e899666a275d221a6919997c79045181d
Andrew Henry <imandrewh@outlook.com>
2020-06-18 22:30:24 -0400

    Updates

---
+++ precache-manifest.206369bba32502d20779ad10a08f79ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77acb3b13638ca84360c1074474e50f",
+    "url": "/react-google-slides/index.html"
+  },
+  {
+    "revision": "ee857880261d82bd0e07",
+    "url": "/react-google-slides/static/css/main.03fb46fd.chunk.css"
+  },
+  {
+    "revision": "f936fd3adba91df3515e",
+    "url": "/react-google-slides/static/js/2.a20ebcbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-google-slides/static/js/2.a20ebcbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee857880261d82bd0e07",
+    "url": "/react-google-slides/static/js/main.a61ac951.chunk.js"
+  },
+  {
+    "revision": "c96936119f0fad1accee",
+    "url": "/react-google-slides/static/js/runtime-main.b0cd3509.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33828fa9eeebd0ab893706a38c7377c0.js"
+  "/react-google-slides/precache-manifest.206369bba32502d20779ad10a08f79ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-google-slides/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
